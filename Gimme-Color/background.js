19f58c2c687f2b9b5797de20e667b53a92327cce
chrisreylo73 <chrisreylo73@gmail.com>
2023-07-05 00:58:48 -0600

    main color picking works and clipboard functionality added

--- Gimme-Color/background.js
+++ Gimme-Color/background.js
@@ -1,5 +1,18 @@
-   chrome.action.onClicked.addListener(function(){
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-         chrome.tabs.sendMessage(tabs[0].id, { message: 'iconClicked' });
-      });
-   });
+chrome.action.onClicked.addListener(function () {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, { message: "iconClicked" });
+	});
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	switch (request.message) {
+		case "getScreenshot":
+			chrome.tabs.captureVisibleTab(null, { format: "png" }, function (dataUrl) {
+				console.log(dataUrl);
+				sendResponse({ screenshot: dataUrl });
+			});
+			return true; // Ensure that sendResponse() is called asynchronously
+		default:
+			break;
+	}
+});
--- Gimme-Color/content.js
+++ Gimme-Color/content.js
@@ -1,5 +1,4 @@
-
-
+//overlay to prevent clicking action
 const overlay = document.createElement("div");
 overlay.style.position = "fixed";
 overlay.style.top = "0";
@@ -8,59 +7,75 @@ overlay.style.width = "100%";
 overlay.style.height = "100%";
 overlay.style.zIndex = "9999";
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-   switch (request.message) {
-      case "iconClicked":
-         console.log('Message received in content:', request.message);
-         document.body.style.cursor = "crosshair";
-         // document.body.appendChild(overlay);
-         break;
-      default:
-         break;
-   }
+// Listens for messages from background
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	switch (request.message) {
+		case "iconClicked":
+			console.log("Message received in content:", request.message);
+			document.body.style.cursor = "crosshair";
+			document.body.appendChild(overlay);
+			document.addEventListener("click", clickHandler);
+			break;
+		default:
+			break;
+	}
 });
 
+function clickHandler(event) {
+	console.log("Click event!");
+	document.body.style.cursor = "default";
+	const x = event.clientX;
+	const y = event.clientY;
+	getPixelColor(x, y);
+	document.body.removeChild(overlay);
+	document.removeEventListener("click", clickHandler);
+}
 
+function getPixelColor(x, y) {
+	chrome.runtime.sendMessage({ message: "getScreenshot" }, function (response) {
+		const screenshot = response.screenshot;
+		var canvas = document.createElement("canvas");
+		var context = canvas.getContext("2d");
+		var img = new Image();
+		img.src = screenshot;
+		// Wait for the image to load before cropping it
+		img.onload = function () {
+			try {
+				canvas.width = img.width;
+				canvas.height = img.height;
+				let xScale = img.width / window.innerWidth;
+				let yScale = img.height / window.innerHeight;
+				context.drawImage(img, 0, 0);
+				x = (x - window.pageXOffset) * xScale;
+				y = (y - window.pageYOffset) * yScale;
 
+				const pixelData = context.getImageData(x, y, 1, 1).data;
+				const rgbaColor = `rgba(${pixelData[0]}, ${pixelData[1]}, ${pixelData[2]}, ${pixelData[3]})`;
+				const hexColor = getHexValue(rgbaColor);
+				console.log(rgbaColor);
+				console.log(hexColor);
+				saveToClipboard(hexColor);
+				// updateExtensionIcon(hexColor);
+			} catch (error) {
+				console.log(error, "error occurred!");
+			}
+		};
+	});
+}
 
-document.addEventListener("click", function (event) {
-   console.log("Click event!");
-   document.body.style.cursor = "default";
-   const x = event.clientX;
-   const y = event.clientY;
-   const pixelColor = getPixelColor(x, y);
-   console.log('Pixel color:', pixelColor);
-   // document.body.removeChild(overlay);
-})
+function getHexValue(rgbaColor) {
+	const rgbaValues = rgbaColor.slice(5, -1).split(",");
+	const red = parseInt(rgbaValues[0].trim());
+	const green = parseInt(rgbaValues[1].trim());
+	const blue = parseInt(rgbaValues[2].trim());
 
- 
- function getPixelColor(x, y) {
-   const canvas = document.createElement('canvas');
-   const context = canvas.getContext('2d');
-   const pixelData = new Uint8Array(4);
- 
-   canvas.width = 1;
-   canvas.height = 1;
-   context.drawImage(document.documentElement, -x, -y, 1, 1, 0, 0, 1, 1);
-   context.getImageData(0, 0, 1, 1).data.set(pixelData);
- 
-   const rgbaColor = `rgba(${pixelData[0]}, ${pixelData[1]}, ${pixelData[2]}, ${pixelData[3]})`;
-   return rgbaColor;
- }
+	const hexRed = red.toString(16).padStart(2, "0");
+	const hexGreen = green.toString(16).padStart(2, "0");
+	const hexBlue = blue.toString(16).padStart(2, "0");
+	const hexColor = `#${hexRed}${hexGreen}${hexBlue}`;
+	return hexColor;
+}
 
-
-
-
-
-
-// const cursor = () => {
-
-// };
-
-// const clipboard = () => {
-
-// };
-
-// const hexValue = () => {
-
-// };
+function saveToClipboard(text) {
+	navigator.clipboard.writeText(text);
+}
